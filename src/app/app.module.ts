eec1573547b6321c814ff6bc1e3c55180f515027
Dionnys Santos Marinho <dionnys.santos@ulbra.edu.br>
2018-04-27 17:30:05 -0300

    Tela formulário cadastro caso público

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import {MaterialModule} from './material.module';
-
-
+import { FormularioPublicoComponent } from './formulario-publico/formulario-publico.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioPublicoComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+      NoopAnimationsModule,
+      MatButtonModule,
+      MatCheckboxModule
 
   ],
   providers: [],
---
+++ src/app/formulario-publico/formulario-publico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-publico',
+  templateUrl: './formulario-publico.component.html',
+  styleUrls: ['./formulario-publico.component.css']
+})
+export class FormularioPublicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
